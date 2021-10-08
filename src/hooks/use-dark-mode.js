08f6179b0d264346f367d9fb99b5172c66400d59
Brian Siklinski <bszyk.codes@gmail.com>
2021-10-08 19:02:19 -0400

    check darkMode hook for window

--- src/hooks/use-dark-mode.js
+++ src/hooks/use-dark-mode.js
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react';
 /**
  * Custom hook for dark mode functionality.
  * @param {boolean} defaultValue Default mode.
- * 
+ *
  * @returns {Array<Boolean, Function>}
  */
 export const useDarkMode = (defaultValue = false) => {
 	const key = 'darkMode';
 
-	const stored = localStorage.getItem('darkMode');
+	const stored =
+		typeof window !== 'undefined' ? localStorage.getItem('darkMode') : null;
 	const initial = stored ? JSON.parse(stored) : defaultValue;
 	const [value, setValue] = useState(initial);
 
