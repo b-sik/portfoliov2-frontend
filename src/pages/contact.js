3b6685e87ea4f1ec061726a311cf5c662729d03a
Brian Siklinski <bszyk.codes@gmail.com>
2021-10-12 10:26:51 -0400

    add contact form & contact page updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Layout from '../components/layout';
 import { contactConfig } from '../config/contact-config';
 import { __ } from '@wordpress/i18n';
+import ContactForm from '../components/contact-form';
 
 const Contact = () => {
 	const currentEmail = 'bszyk.codes@gmail.com';
@@ -9,67 +10,60 @@ const Contact = () => {
 	/**
 	 * Gradient.
 	 */
-	const bgGradient = 'bg-gradient-to-b from-indigo-300 to-white dark:from-indigo-500 dark:to-black';
+	const bgGradient =
+		'bg-gradient-to-b from-indigo-300 to-white dark:from-indigo-500 dark:to-black';
 
 	return (
 		<Layout location="/contact">
 			<div
 				id="contact"
-				className={`${bgGradient} w-full h-screen overflow-hidden grid grid-cols-12 grid-rows-auto place-items-center col-span-full relative`}
+				className={`${bgGradient} w-full h-auto overflow-hidden grid grid-cols-12 grid-rows-auto place-items-center col-span-full pb-4`}
 			>
-				<h2 className="row-start-1 row-end-2 col-span-full">
-					{__('Contact', 'bszyk-portfolio')}
-				</h2>
-				<ul className="row-start-2 row-end-3 text-white flex flex-col sm:flex-row justify-center text-center content-center col-span-full">
-					{contactConfig.map((link, i, arr) => {
+				<div className="row-start-1 row-end-2 col-span-full my-4 md:my-0">
+					<h3 className="text-center">
+						{__('Contact', 'bszyk-portfolio')}
+					</h3>
+					<p className="text-center">
+						{__("Let's work together!", 'bszyk-portfolio')}
+					</p>
+				</div>
+				<div className="col-start-2 col-end-12 row-start-2 row-end-3 col-span-full md:col-start-3 md:col-end-6 w-full">
+					<ContactForm />
+				</div>
+				<ul className="mt-8 md:mt-0 row-start-3 row-end-4 md:row-start-2 md:row-end-3 text-white flex flex-col justify-center text-center content-center col-span-full md:col-start-8 md:col-end-11 w-full">
+					{contactConfig.map((link, i) => {
 						const { name, url, icon } = link;
 
 						return (
-							<Fragment key={i}>
-								<li
-									key={i}
-									className={`my-4 hover:animate-hover-grow`}
+							<li
+								key={i}
+								className={`brand-icon-list-item my-4 hover:animate-hover-grow border rounded-xl p-2 mx-4 md:mx-0 bg-white bg-opacity-50`}
+							>
+								<a
+									href={
+										name === 'email'
+											? `mailto:${currentEmail}`
+											: url
+									}
+									target="_blank"
+									rel="noreferrer noopener"
+									className={`text-2xl text-black dark:text-white brand-icon-anchor cursor-pointer`}
 								>
-									<a
-										key={`anchor-${i}`}
-										href={
-											name === 'email'
-												? `mailto:${currentEmail}`
-												: url
-										}
-										target="_blank"
-										rel="noreferrer noopener"
-										className={`text-2xl text-black dark:text-white brand-icon-anchor cursor-pointer`}
+									{
+										// @see '../css/icons.css'
+									}
+									<span
+										className={`brand-icon-${name.toLowerCase()} ${
+											name === 'email' &&
+											`hover:text-red-500`
+										}`}
 									>
-										{
-											// @see '../css/brand-icons.css'
-										}
-										<span
-											key={`icon-${i}`}
-											className={`brand-icon-${name.toLowerCase()} ${
-												name === 'email' &&
-												`hover:text-red-500`
-											}`}
-										>
-											{icon}
-										</span>
-										&nbsp;&nbsp;
-										<span key={`name-${i}`}>
-											{name === 'email'
-												? currentEmail
-												: name}
-										</span>
-									</a>
-								</li>
-								{i !== arr.length - 1 && (
-									<li
-										key={`divider-${i}`}
-										className={`my-4 mx-4 text-2xl text-black dark:text-white hidden sm:inline`}
-									>
-										|
-									</li>
-								)}
-							</Fragment>
+										{icon}
+									</span>
+									&nbsp;&nbsp;
+									<span>{name}</span>
+								</a>
+							</li>
 						);
 					})}
 				</ul>
