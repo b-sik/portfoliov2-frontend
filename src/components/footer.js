74f87ca3dfce52f0cf601ec0c0874e0ae8127166
Brian Siklinski <bszyk.codes@gmail.com>
2021-10-13 14:54:08 -0400

    add footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,96 +5,100 @@ import Icons from '../components/icons';
 
 const Footer = ({ location }) => {
 	const { socialLinks } = useContactPageData();
+	const linkClassnames = 'text-blue-700 dark:text-blue-200';
 
 	return (
-		<>
-			<footer className="py-2 bg-white dark:bg-black overflow-hidden relative text-center">
-				<FooterNav location={location} />
-				<div className="flex flex-col sm:flex-row place-content-center">
-					<div id="footer-copyright-div flex place-content-center">
-						<span className="text-xs sm:text-sm text-black dark:text-white leading-none my-auto">{`© ${new Date().getFullYear()} Brian Siklinski. `}</span>
-						<br className="inline sm:hidden" />
-						<span className="text-xs sm:text-sm text-black dark:text-white leading-none my-auto">
-							Created with{' '}
-							<a
-								href="https://reactjs.org/"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								React
-							</a>
-							,&nbsp;
-							<a
-								href="https://www.gatsbyjs.com/"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								Gatsby
-							</a>
-							,&nbsp;
-							<a
-								href="https://wordpress.org/"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								Wordpress
-							</a>
-							,&nbsp;
-							<a
-								href="https://tailwindcss.com/"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								TailWindCSS
-							</a>
-						</span>
-						<span
-							className={`text-lg text-black dark:text-white ml-2 ${
-								location === '/contact'
-									? 'hidden'
-									: 'hidden md:inline'
-							}`}
+		<footer className="py-2 bg-white dark:bg-black overflow-hidden relative text-center">
+			<FooterNav location={location} />
+			<div className="flex flex-col sm:flex-row place-content-center">
+				<div id="footer-copyright-div flex place-content-center">
+					<span className="text-xs sm:text-sm text-black dark:text-white leading-none my-auto">{`© ${new Date().getFullYear()} Brian Siklinski. `}</span>
+					<br className="inline sm:hidden" />
+					<span className="text-xs sm:text-sm text-black dark:text-white leading-none my-auto">
+						Created with&nbsp;
+						<a
+							href="https://reactjs.org/"
+							target="_blank"
+							rel="noopener noreferrer"
+							className={linkClassnames}
 						>
-							{' '}
-							|{' '}
-						</span>
-					</div>
-					<ul
-						className={`${
-							location === '/contact' ? 'hidden' : 'inline'
-						} w-auto mx-auto sm:mx-0 flex justify-items-center order-first sm:order-none`}
+							React
+						</a>
+						,&nbsp;
+						<a
+							href="https://www.gatsbyjs.com/"
+							target="_blank"
+							rel="noopener noreferrer"
+							className={linkClassnames}
+						>
+							Gatsby
+						</a>
+						,&nbsp;
+						<a
+							href="https://wordpress.org/"
+							target="_blank"
+							rel="noopener noreferrer"
+							className={linkClassnames}
+						>
+							Wordpress
+						</a>
+						,&nbsp;&&nbsp;
+						<a
+							href="https://tailwindcss.com/"
+							target="_blank"
+							rel="noopener noreferrer"
+							className={linkClassnames}
+						>
+							TailwindCSS
+						</a>
+						.
+					</span>
+					<span
+						className={`text-lg text-black dark:text-white ${
+							location === '/contact'
+								? 'hidden'
+								: 'hidden md:inline'
+						}`}
 					>
-						{Object.entries(socialLinks).map((link, i) => {
-							const { name, url, icon } = link[1];
-
-							return (
-								<li
-									key={i}
-									className="mx-2 cursor-pointer brand-icon-list-item"
-								>
-									<a
-										href={url}
-										target="_blank"
-										rel="noreferrer noopener"
-										className={`text-lg text-black dark:text-white`}
-									>
-										<span
-											className={`brand-icon-${name.toLowerCase()} ${
-												link[0] === 'email'
-													? `hover:text-red-500`
-													: ''
-											}`}
-										>
-											<Icons iconName={icon} />
-										</span>
-									</a>
-								</li>
-							);
-						})}
-					</ul>
+						{' '}
+						|{' '}
+					</span>
 				</div>
-			</footer>
-		</>
+				<ul
+					className={`${
+						location === '/contact' ? 'hidden' : 'inline'
+					} w-auto mx-auto sm:mx-0 flex justify-items-center order-first sm:order-none`}
+				>
+					{Object.entries(socialLinks).map((link, i) => {
+						const { name, url, icon } = link[1];
+
+						return (
+							<li
+								key={i}
+								className="mx-2 cursor-pointer brand-icon-list-item"
+							>
+								<a
+									href={url}
+									target="_blank"
+									rel="noreferrer noopener"
+									className={`text-lg text-black dark:text-white`}
+								>
+									<span
+										className={`brand-icon-${name.toLowerCase()} ${
+											link[0] === 'email'
+												? `hover:text-red-500`
+												: ''
+										}`}
+									>
+										<Icons iconName={icon} />
+									</span>
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</footer>
 	);
 };
 
