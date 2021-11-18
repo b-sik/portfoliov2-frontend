bf5bee4b7d1075387ce3add4321d6e9493bf173b
Brian Siklinski <bszyk.codes@gmail.com>
2021-11-17 21:30:28 -0500

    responsive section wrapper

--- src/components/layout/section-wrapper.js
+++ src/components/layout/section-wrapper.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { processFeaturedImg } from '../utilities/process-img';
 
-
-const SectionWrapper = ({ id, children, featuredImage }) => {
-
-	// const bgImgProps = 'bg-auto bg-left-top bg-no-repeat';
-
+const SectionWrapper = ({ id, children }) => {
 	return (
 		<section
 			id={id}
-			className={`col-span-full border grid grid-cols-12 grid-rows-auto m-5`}
+			className={`col-span-full border grid grid-cols-12 grid-rows-auto md:m-5`}
 			style={{
 				minHeight: '90vh',
-				// backgroundImage: bgImgSrc ? `url(${bgImgSrc})` : '',
 			}}
 		>
 			{children}
