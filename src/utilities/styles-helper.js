fcaf54fae62675e33c8243713ed028ebe62c79a1
Brian Siklinski <bszyk.codes@gmail.com>
2021-11-19 10:36:53 -0500

    simplify stylesHelper

--- src/utilities/styles-helper.js
+++ src/utilities/styles-helper.js
@@ -1,32 +1,12 @@
 /**
  * Utility to help organize Tailwind CSS classes when it becomes overwhelming for a component.
- *
- * Organize your classes in a simple object, e.g.
- * ```
- * let componentStyles = {
- *  dimensions: 'w-80 h-50',
- *  spacing: 'mt-4 p-2',
- *  etc...
- * }
- * ```
  * @param {Object<string>} styles
  * @returns {string} Single string of all CSS classes.
  */
 const stylesHelper = (styles) => {
-	const helper = {
-		styles: null,
-		get all() {
-			return null !== this.styles
-				? Object.values(this.styles)
-						.map((value) => value)
-						.join(' ')
-				: '';
-		},
-	};
-	helper.styles = styles;
-	styles = helper.all;
-
-	return styles;
+	return Object.values(styles)
+		.map((value) => value)
+		.join(' ');
 };
 
 export default stylesHelper;
