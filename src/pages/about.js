d2d3a6805ab54458a1ce3a0c6f86393cdef59b19
Brian Siklinski <bszyk.codes@gmail.com>
2021-11-19 11:59:47 -0500

    delete front-page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
 import Layout from '../components/layout/layout';
 import AboutSection from './../components/sections/about-section';
 import SkillsSection from './../components/sections/skills-section';
+import Seo from '../components/seo';
 
 const About = (props) => {
+	/**
+	 * Navigate to '/home' if pathname is '/'
+	 */
+	useEffect(() => {
+		if ('/' === props.location.pathname) {
+			navigate('/about');
+			return <Seo />;
+		}
+		// 	eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<Layout {...props}>
