b6e49093172dd4c3cd25a89b3a1d100b615f6258
Brian Siklinski <bszyk.codes@gmail.com>
2021-11-30 18:01:01 -0500

    update nav colors

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { __ } from '@wordpress/i18n';
 import { stripTrailingSlash } from '../../utilities/strip-trailing-slash';
-import NavToolbar from './nav-toolbar';
+// import NavToolbar from './nav-toolbar';
 
 const config = [
 	{
@@ -22,31 +22,27 @@ const config = [
 const Nav = ({ pathname }) => {
 	return (
 		<nav
-			className={`bg-indigo-500 dark:bg-indigo-900 border-b-1 w-full overflow-hidden flex-grow-0 relative dark:opacity-50 shadow-xl`}
+			className={` bg-black bg-opacity-90 border-b-1 w-full overflow-hidden flex-grow-0 relative shadow-xl`}
 		>
 			<ul className="flex justify-around content-center">
 				{config.map((page, i) => {
 					const { name, slug } = page;
+
 					const linkClassName = `inline-block ${
 						stripTrailingSlash(pathname) === slug
-							? 'cursor-default'
-							: 'hover:bg-gray-200 hover:opacity-70'
-					}  py-2 px-4 h-full border rounded border-indigo-500 dark:border-indigo-900 hover:border-gray-200 dark:hover:border-gray-200`;
-					const hClassName = `${
-						stripTrailingSlash(pathname) === slug
-							? 'text-gray-200 dark:text-black'
-							: 'text-black dark:text-gray-200 dark:hover:text-black'
-					}`;
+							? 'cursor-default hover:border-gray-200'
+							: 'hover:bg-indigo-500 hover:border-black'
+					}  py-2 px-4 h-full border rounded border-indigo-500`;
 
 					return (
 						<li key={i} className="my-2">
 							<Link className={linkClassName} to={slug}>
-								<h6 className={hClassName}>{name}</h6>
+								<h6 className='text-white'>{name}</h6>
 							</Link>
 						</li>
 					);
 				})}
-				<NavToolbar />
+				{/* <NavToolbar /> */}
 			</ul>
 		</nav>
 	);
