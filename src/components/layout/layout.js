4622ed22d6f4ff7600e715c4198d95dc975fa593
Brian Siklinski <bszyk.codes@gmail.com>
2021-11-30 18:00:34 -0500

    disable footer nav

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from '../nav/nav';
 import Seo from '../seo';
 import Footer from '../footer/footer';
-import FooterNav from '../footer/footer-nav';
+// import FooterNav from '../footer/footer-nav';
 
 const Layout = ({ location: { pathname }, children }) => {
 
@@ -11,14 +11,14 @@ const Layout = ({ location: { pathname }, children }) => {
 			<Seo />
 			<div
 				id="site-container"
-				className={`bg-white dark:bg-black dark:text-gray-300 flex-wrap grid grid-cols-1 auto-rows-min`}
+				className={`bg-gray-100 dark:bg-black dark:text-gray-300 flex-wrap grid grid-cols-1 auto-rows-min`}
 			>
 				<Nav pathname={pathname} />
 				<main
 					className={`flex-wrap h-auto grid grid-cols-1 grid-rows-auto`}
 				>
 					{children}
-					<FooterNav pathname={pathname} />
+					{/* <FooterNav pathname={pathname} /> */}
 				</main>
 				<Footer pathname={pathname} />
 			</div>
