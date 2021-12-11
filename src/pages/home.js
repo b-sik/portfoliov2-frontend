12ffde1ff475bbefaa189a00e4749b7f6bee9511
Brian Siklinski <bszyk.codes@gmail.com>
2021-12-11 11:37:39 -0500

    remove unneeded redirect

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,24 +1,11 @@
-import React, { useEffect } from 'react';
-import { navigate } from 'gatsby';
+import React from 'react';
 import Layout from '../components/layout/layout';
 import HomeSection from './../components/sections/home-section';
 import SkillsSection from './../components/sections/skills-section';
 import ProjectsSection from './../components/sections/projects-section';
 import ContactSection from './../components/sections/contact-section';
-import Seo from '../components/seo';
 
 const Home = (props) => {
-	/**
-	 * Navigate to '/home' if pathname is '/'
-	 */
-	useEffect(() => {
-		if ('/' === props.location.pathname) {
-			navigate('/home');
-			return <Seo />;
-		}
-		// 	eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
 	return (
 		<Layout {...props}>
 			<HomeSection />
